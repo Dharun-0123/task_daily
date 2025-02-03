d033843b990b25a642581215b009190941730602
Dharun-0123 <dharunkavin123@outlook.com>
2025-02-03 18:12:29 +0000

    finished

--- toggle_on/off/script.js
+++ toggle_on/off/script.js
@@ -1,5 +1,10 @@
 let toggler =document.getElementById('switch');
 
 toggler.addEventListener("click",()=>{
-console.log(toggler.checked);
+if(toggler.checked === true){
+    document.body.style.backgroundColor="black";}
+    else{
+        document.body.style.backgroundColor="white";
+    }
+
 });
